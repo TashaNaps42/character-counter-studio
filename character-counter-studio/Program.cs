122b6c8dd5b235339cbe8a7edec2fa8ced750a3f
Tasha L <natasha.lewis.lalor@gmail.com>
2022-08-10 19:59:39 -0500

    This one can be used on computers other than Tashas

--- character-counter-studio/Program.cs
+++ character-counter-studio/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            string path = "C:/Users/Naps/Documents/launchcode/c-sharp/shortname.txt";
-            string userString = File.ReadAllText(path);
+            //string path = "C:/Users/Naps/Documents/launchcode/c-sharp/shortname.txt";
+            //string userString = File.ReadAllText(path);
             //string loremPrime = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
             Dictionary<char, int> CharCount = new Dictionary<char, int>();
-            //Console.WriteLine("Enter a string, or sentence: ");
-            //string userString = Console.ReadLine().ToLower();
+            Console.WriteLine("Enter a string, or sentence: ");
+            string userString = Console.ReadLine().ToLower();
 
             foreach (char c in userString)
             {
